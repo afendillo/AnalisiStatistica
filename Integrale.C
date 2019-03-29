40f83a5bfa01a31a72a1f47e301f826ecffdc8f6
Alessandro Fendillo <alessandrofendillo@gmail.com>
2019-03-29 21:53:46 +0100

    Corrections to Integrale.C

--- Integrale.C
+++ Integrale.C
@@ -1,160 +1,124 @@
 #define NRand 1e6
-#define NInt 2500
-#define bin 200
+#define NInt 1e3
+#define bin 50
 #define xmax 5
 #define xmin 0
 #define Range xmax-xmin
 
 
 using namespace std;
-
-
-
-
-//Poly function
-double poly (double x) {
-
-	double y = x*x*x*(x*(3*x + 2) + 3);
-	return y;
-}
-
-//Trial distribution
-double trial (double x) {
-
-	double y = x*x*x*4;
-	return y;
-}
-
-//Cumulante
-double cumul (double x) {
-
-	double y = x*x*x*x;
-	return y;
-}
-
-//Cumulante invertita
-double lumuc (double x) {
-
-	double y = pow(x, 1./4.);
-	return y;
-}
-
-//Constant PDFunction a-b interval
-double cFunc(double x) {
-		return ROOT::Math::uniform_pdf  (x,0,1,0);
-							
-}	
-//integrale montecarlo distribuzione uniforme
-double integral (double x) {
-
-	double I=0;
-	for(int i = 0 ; i<NRand; i++)
-		{
-			I = I + poly(x);
-		}
-	return (Range)*I/NRand;
-}
-//integrale montecarlo distribuzione alternativa
-double integral2 (double x) {
-
-	double I=0;
-	for(int i = 0 ; i<NRand; i++)
-		{
-			I = I + poly(x)/trial(x);
-		}
-	return cumul(xmax)*I/NRand;
-}
-void Integrale(char stamp='Q')
-{	
+void Reset(){
 	//Recupera la lista di tutti i canvas e li cancella
-	int count=0;
 	TSeqCollection* canvases = gROOT->GetListOfCanvases();
 	TIter next(canvases);
 	while(TCanvas *c = (TCanvas*)next())
 	{
 		delete c;
-		count++;
 	}
-	if(stamp!='Q') cout<<count<<" Canvases Deleted"<<endl;
 	//Cancella tutti gli oggetti creati
 	//Necessario per evitare "warning <TROOT::Append>: Replacing existing TH1: <Name> (Potential memory leak)"
 	gROOT->DeleteAll();
-	
-	//Informazioni statistiche da stampare
-	gStyle->SetOptFit(1111);
+	return;
+}
+//Poly function
+double poly (double x) {
+
+	return x*x*x*(x*(3*x + 2) + 3);
+}
+
+//Trial distribution
+double trial (double x) {
+	return  x*x*x*4;
+}
+
+//Cumulante
+double cumul (double x) {
+	return x*x*x*x;
+}
+
+//Cumulante invertita
+double lumuc (double x) {
+
+	return pow(x, 1./4.);
+}
+
+//Constant PDFunction a-b interval
+double cFunc(double x) {
+		return ROOT::Math::uniform_pdf  (x,0,1,0);						
+}	
+//integrale montecarlo distribuzione uniforme
+// double integral (double* rand) {
+
+// 	double I=0, x;
+// 	for(int i = 0 ; i<NRand; i++)
+// 		{
+// 			x=rand[i]*(Range)+xmin;
+// 			I = I + poly(x);
+// 		}
+// 	return (Range)*I/NRand;
+// }
+//integrale montecarlo distribuzione alternativa
+// double integral2 (double* rand) {
+
+// 	double ymin = cumul(xmin), ymax = cumul(xmax);
+// 	double I=0, x;
+// 	for(int i = 0 ; i<NRand; i++)
+// 		{
+// 			x=lumuc(rand[i]*(ymax-ymin)+ymin);
+// 			I = I + poly(x)/trial(x);
+// 		}
+// 	return cumul(xmax)*I/NRand;
+// }
+void Integrale()
+{	
+	Reset();
 
 //metodo 1
 
 	TRandom3 *RandGenerator = new TRandom3(time(0));
-	double Result[NInt];
-	double Rmin = 0, Rmax = 0;
-
-	
-	for (int i = 0; i<NInt; i++)
-	{
-		Result[i] = integral(RandGenerator->Rndm()*(Range)+xmin);
-		//cout<<Result[i]<<endl;
-
-		if(Result[i]>Rmax) {
-			Rmax=Result[i];}
-
-		if(Rmin==0) {
-			Rmin=Result[i];}
-		else if(Rmin>Result[i]){
-			Rmin=Result[i];}
-	}
+	double Result , Rmin = 0, Rmax = 0 , I ,J, x , y;
+	double Result2, ymin = cumul(xmin), ymax = cumul(xmax);
 
 	TH1D *HistoInt = new TH1D ("HistoInt" , "Uniforme" , bin ,Rmin, Rmax);
 	HistoInt->GetXaxis()->SetTitle("Area");
 	HistoInt->GetYaxis()->SetTitle("Conteggi");
 	HistoInt->SetFillColorAlpha(kGreen, 0.30);
 
+	TH1D *HistoInt2 = new TH1D ("HistoInt2" , "Cubica" , bin ,Rmin, Rmax);
+	HistoInt2->GetXaxis()->SetTitle("Area");
+	HistoInt2->GetYaxis()->SetTitle("Conteggi");
+	HistoInt2->SetFillColorAlpha(kGreen, 0.30);
+	cout<<"\nStarting....\n";
 	for (int i = 0; i<NInt; i++)
 	{
-		HistoInt->Fill(Result[i]);
-	}
+		I=0;J=0;
+		if(i%(int)(NInt/100)==0) 
+		{
+			for (int k = 0 ; k<i/(int)(NInt/10)+1; k++) cout << "*" ;
+			cout<<" "<< i/(int)(NInt/100)<< "% \r";
+			cout<<flush;
+		}
+		for(int j = 0 ; j<NRand; j++)
+		{
+			
+			x=(RandGenerator->Rndm())*(Range)+xmin;
+			I = I + poly(x);
 
+			y=lumuc((RandGenerator->Rndm())*(ymax-ymin)+ymin);
+			J = J + poly(y)/trial(y);
+		}
+		HistoInt2->Fill( cumul(xmax)*J/NRand);
+		HistoInt->Fill((Range)*I/NRand);
+	}
+	cout<<endl;
 
 	TCanvas *c1 = new TCanvas();
 	c1->SetGrid();
 	HistoInt->Draw();
 
-//metodo 2
-
-	double Result2[NInt];
-	double ymin = cumul(xmin), ymax = cumul(xmax);
-	Rmin = 0;
-	Rmax = 0;
-	
-	for (int i = 0; i<NInt; i++)
-	{
-		Result2[i] = integral2(lumuc(RandGenerator->Rndm()*(ymax-ymin)+ymin));
-		//cout<<Result[i]<<endl;
-
-		if(Result2[i]>Rmax) {
-			Rmax=Result2[i];}
-
-		if(Rmin==0) {
-			Rmin=Result2[i];}
-		else if(Rmin>Result2[i]){
-			Rmin=Result2[i];}
-	}
-
-	TH1D *HistoInt2 = new TH1D ("HistoInt2" , "Cubica" , bin ,Rmin, Rmax);
-	HistoInt2->GetXaxis()->SetTitle("Area");
-	HistoInt2->GetYaxis()->SetTitle("Conteggi");
-	HistoInt2->SetFillColorAlpha(kGreen, 0.30);
-
-	for (int i = 0; i<NInt; i++)
-	{
-		HistoInt2->Fill(Result2[i]);
-	}
-
-
 	TCanvas *c2 = new TCanvas();
 	c2->SetGrid();
 	HistoInt2->Draw();
-	
 
 	return;	
 }
